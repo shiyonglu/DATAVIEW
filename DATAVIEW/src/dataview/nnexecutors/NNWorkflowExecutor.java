5bafd20d6d5a89dc402423a5f3c3c74b5405d0fc
ChangxinBai <baichangxin@gmail.com>
2022-06-10 11:11:09 -0400

    Update NNWorkflowExecutor.java

--- DATAVIEW/src/dataview/nnexecutors/NNWorkflowExecutor.java
+++ DATAVIEW/src/dataview/nnexecutors/NNWorkflowExecutor.java
@@ -10,18 +10,19 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class NNWorkflowExecutor {
-	protected JSONObject configuration;
+	//protected JSONObject configuration;
 	protected String NNWorkflowExecutor_type;
 	public NNWorkflowExecutor_base newClass;
 	
 	public NNWorkflowExecutor(String inputFileName, String jsonFileName, String location) {
-		this.configuration = fetchConfiguration();
-		try {
-			this.NNWorkflowExecutor_type = configuration.getJSONObject("NNExecutor").getString("type");
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
+		/*
+		 * this.configuration = fetchConfiguration(); try { this.NNWorkflowExecutor_type
+		 * = configuration.getJSONObject("NNExecutor").getString("type"); } catch
+		 * (JSONException e) { // TODO Auto-generated catch block e.printStackTrace(); }
+		 */
+		
+		this.NNWorkflowExecutor_type = "LocalGPU";
 		
 		switch(this.NNWorkflowExecutor_type) {
 		case "LocalGPU":
@@ -33,27 +34,19 @@ public class NNWorkflowExecutor {
 		}
 	}
 	
-	public JSONObject fetchConfiguration() {
-		String configure = ""; 
-		JSONObject configureJson; 
-		String configRepo = System.getProperty("user.dir") + File.separator + "WebContent";
-		Path path  = Paths.get(configRepo);  
-		try {
-			configure = new String(Files.readAllBytes(Paths.get(path+ File.separator+"config.json")));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		JSONObject obj = null;
-		try {
-			obj = new JSONObject(configure);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return obj;
-	}
+	/*
+	 * public JSONObject fetchConfiguration() { String configure = ""; JSONObject
+	 * configureJson; String configRepo = System.getProperty("user.dir") +
+	 * File.separator + "WebContent"; //String configRepo = location +
+	 * File.separator + "WebContent"; Path path = Paths.get(configRepo); try {
+	 * configure = new String(Files.readAllBytes(Paths.get(path+
+	 * File.separator+"config.json"))); } catch (IOException e) { // TODO
+	 * Auto-generated catch block e.printStackTrace(); } JSONObject obj = null; try
+	 * { obj = new JSONObject(configure); } catch (JSONException e) { // TODO
+	 * Auto-generated catch block e.printStackTrace(); }
+	 * 
+	 * return obj; }
+	 */
 }
 
 
